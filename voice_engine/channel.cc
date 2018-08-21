fd848e635f7a2e9e85b1ee603e478b9705cea73d
Dan Minor <dminor@mozilla.com>
2018-08-21 13:39:00 -0400

    Bug 1376873 - Use Call interface in AudioConduit; r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D7441
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/5d2899adc3057fe4df9dd454887c1832f54d91d9

--- voice_engine/channel.cc
+++ voice_engine/channel.cc
@@ -1474,6 +1474,18 @@ int Channel::SetReceiveAudioLevelIndicationStatus(bool enable,
   return 0;
 }
 
+int Channel::SetReceiveCsrcAudioLevelIndicationStatus(bool enable,
+                                                      unsigned char id) {
+  rtp_header_parser_->DeregisterRtpHeaderExtension(kRtpExtensionCsrcAudioLevel);
+  if (enable &&
+      !rtp_header_parser_->RegisterRtpHeaderExtension(kRtpExtensionCsrcAudioLevel,
+                                                      id)) {
+    return -1;
+  }
+  return 0;
+}
+
+
 void Channel::EnableSendTransportSequenceNumber(int id) {
   int ret =
       SetSendRtpHeaderExtension(true, kRtpExtensionTransportSequenceNumber, id);
@@ -1495,6 +1507,10 @@ void Channel::RegisterSenderCongestionControlObjects(
   TransportFeedbackObserver* transport_feedback_observer =
       transport->transport_feedback_observer();
   PacketRouter* packet_router = transport->packet_router();
+  //This allows us to re-create streams but keep the same channel
+  if (packet_router_ == packet_router) {
+    return;
+  }
 
   RTC_DCHECK(rtp_packet_sender);
   RTC_DCHECK(transport_feedback_observer);
@@ -1514,6 +1530,11 @@ void Channel::RegisterSenderCongestionControlObjects(
 void Channel::RegisterReceiverCongestionControlObjects(
     PacketRouter* packet_router) {
   RTC_DCHECK(packet_router);
+  //This allows us to re-create streams but keep the same channel
+  if (packet_router_ == packet_router) {
+    return;
+  }
+
   RTC_DCHECK(!packet_router_);
   constexpr bool remb_candidate = false;
   packet_router->AddReceiveRtpModule(_rtpRtcpModule.get(), remb_candidate);
