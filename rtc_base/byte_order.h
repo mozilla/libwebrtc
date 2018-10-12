ac7bc34a1a0d5805084dfa4bf2fdb397370a3877
Jan Beich <jbeich@FreeBSD.org>
2018-10-12 12:56:00 -0400

    Bug 1376873 - Unbreak WebRTC 64 build on BSDs. r=dminor f=gaston
    Also includes:
    Bug 1554949 - Fix WebRTC build failure with newer linux kernel. r=dminor
    
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/a636ecdcb91afb1c5d436bbcbd87da4f10f7df78

--- rtc_base/byte_order.h
+++ rtc_base/byte_order.h
@@ -90,6 +90,8 @@
 #error WEBRTC_ARCH_BIG_ENDIAN or WEBRTC_ARCH_LITTLE_ENDIAN must be defined.
 #endif  // defined(WEBRTC_ARCH_LITTLE_ENDIAN)
 
+#elif defined(WEBRTC_BSD) && !defined(__OpenBSD__)
+#include <sys/endian.h>
 #elif defined(WEBRTC_POSIX)
 #include <endian.h>
 #else
