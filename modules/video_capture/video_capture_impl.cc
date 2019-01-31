9ed004f7c78840a541f88d3ccc610601bc5798a3
Dan Minor <dminor@mozilla.com>
2019-01-31 15:37:00 -0500

    Bug 1524208 - Calculate stride based upon target_width in video_capture_impl.cc; r=pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D18270
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/51d12094d825c4c4467cb132d03d4f3cad4b1b82

--- modules/video_capture/video_capture_impl.cc
+++ modules/video_capture/video_capture_impl.cc
@@ -173,8 +173,6 @@ int32_t VideoCaptureImpl::IncomingFrame(uint8_t* videoFrame,
     }
   }
 
-  int stride_y = width;
-  int stride_uv = (width + 1) / 2;
   int target_width = width;
   int target_height = abs(height);
 
@@ -189,6 +187,9 @@ int32_t VideoCaptureImpl::IncomingFrame(uint8_t* videoFrame,
       target_height = width;
     }
   }
+
+  int stride_y = target_width;
+  int stride_uv = (target_width + 1) / 2;
 
   // Setting absolute height (in case it was negative).
   // In Windows, the image starts bottom left, instead of top left.
