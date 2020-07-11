5297dafad46010ec7b84abe6a6c815cf9ae7b815
Randell Jesup <rjesup@wgate.com>
2020-07-11 12:31:00 +0000

    Bug 1112392: Move webrtc Tab Sharing to work in e10s/fission r=dminor
    Also we drop support for an independent-of-scroll/viewport capture, which
    the old Tab Sharing supported, for security reasons (and we don't need it).
    
    Differential Revision: https://phabricator.services.mozilla.com/D80974
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/d57a030e6e3ae9ff56f14e8cc732b0e1d3869858

--- modules/desktop_capture/desktop_capturer.h
+++ modules/desktop_capture/desktop_capturer.h
@@ -136,6 +136,10 @@ class RTC_EXPORT DesktopCapturer {
   static std::unique_ptr<DesktopCapturer> CreateScreenCapturer(
       const DesktopCaptureOptions& options);
 
+  // Creates a DesktopCapturer instance which targets to capture tab.
+  static std::unique_ptr<DesktopCapturer> CreateTabCapturer(
+      const DesktopCaptureOptions& options);
+
 #if defined(WEBRTC_USE_PIPEWIRE) || defined(WEBRTC_USE_X11)
   static bool IsRunningUnderWayland();
 #endif  // defined(WEBRTC_USE_PIPEWIRE) || defined(WEBRTC_USE_X11)
@@ -157,6 +161,10 @@ class RTC_EXPORT DesktopCapturer {
   // Creates a platform specific DesktopCapturer instance which targets to
   // capture apps.
   static std::unique_ptr<DesktopCapturer> CreateRawAppCapturer(
+      const DesktopCaptureOptions& options);
+
+  // Creates a DesktopCapturer instance which targets to capture tabs
+  static std::unique_ptr<DesktopCapturer> CreateRawTabCapturer(
       const DesktopCaptureOptions& options);
 };
 
