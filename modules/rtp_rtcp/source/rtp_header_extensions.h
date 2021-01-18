a0c02a048701f66ae907f9fa81f5be213375ddef
Andreas Pehrson <apehrson@mozilla.com>
2021-01-18 11:07:00 +0100

    Bug 1766646 - (fix-ae0d117d51) ifdef our Csrc impl vs upstream's impl, see Bug 1771332.

--- modules/rtp_rtcp/source/rtp_header_extensions.h
+++ modules/rtp_rtcp/source/rtp_header_extensions.h
@@ -94,6 +94,7 @@ class AudioLevel {
                     uint8_t audio_level);
 };
 
+#if !defined(WEBRTC_MOZILLA_BUILD)
 class CsrcAudioLevel {
  public:
   static constexpr RTPExtensionType kId = kRtpExtensionCsrcAudioLevel;
@@ -108,6 +109,7 @@ class CsrcAudioLevel {
   static bool Write(rtc::ArrayView<uint8_t> data,
                     rtc::ArrayView<const uint8_t> csrc_audio_levels);
 };
+#endif
 
 class TransmissionOffset {
  public:
@@ -298,6 +300,7 @@ class ColorSpaceExtension {
   static size_t WriteLuminance(uint8_t* data, float f, int denominator);
 };
 
+#if defined(WEBRTC_MOZILLA_BUILD)
 class CsrcAudioLevel {
  public:
   static constexpr RTPExtensionType kId = kRtpExtensionCsrcAudioLevel;
@@ -312,6 +315,7 @@ class CsrcAudioLevel {
   static size_t ValueSize(const CsrcAudioLevelList& csrcAudioLevels);
   static bool Write(rtc::ArrayView<uint8_t> data, const CsrcAudioLevelList& csrcAudioLevels);
 };
+#endif
 
 // Base extension class for RTP header extensions which are strings.
 // Subclasses must defined kId and kUri static constexpr members.
