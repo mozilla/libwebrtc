e5c1429e0403618099920014584f247a67eaa52f
Byron Campen [:bwc] <docfaraday@gmail.com>
2021-03-12 08:55:00 -0600

    Bug 1654112 - libwebrtc modification: Surface video RTCP SR stats again. r=ng
    libwebrtc has stopped surfacing these, and Chromium does not support
    these stats at all.
    
    Differential Revision: https://phabricator.services.mozilla.com/D108674
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/edac9d01a9ac7594f4b22708a4690753638ca32c

--- video/video_receive_stream2.cc
+++ video/video_receive_stream2.cc
@@ -573,6 +573,14 @@ VideoReceiveStreamInterface::Stats VideoReceiveStream2::GetStats() const {
       }
     }
   }
+
+  // Mozilla modification: VideoReceiveStream2 and friends do not surface RTCP
+  // stats at all, and even on the most recent libwebrtc code there does not
+  // seem to be any support for these stats right now. So, we hack this in.
+  rtp_video_stream_receiver_.RemoteRTCPSenderInfo(
+      &stats.rtcp_sender_packets_sent, &stats.rtcp_sender_octets_sent,
+      &stats.rtcp_sender_ntp_timestamp_ms);
+
   return stats;
 }
 
