ede39c2136bf580a2f2e19ad990de8e1719235a4
Michael Froman <mfroman@mozilla.com>
2021-04-16 17:35:00 -0500

    Bug 1654112 - General build fixes for paths and naming changes. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D113438
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/99b99cca6b7b2c2ebffb5472457a4f927bda11c4

--- modules/video_capture/video_capture_impl.h
+++ modules/video_capture/video_capture_impl.h
@@ -88,6 +88,9 @@ class RTC_EXPORT VideoCaptureImpl : public VideoCaptureModule {
   VideoCaptureImpl();
   ~VideoCaptureImpl() override;
 
+  // moved DeliverCapturedFrame to protected for VideoCaptureAndroid (mjf)
+  int32_t DeliverCapturedFrame(VideoFrame& captureFrame);
+
   char* _deviceUniqueId;  // current Device unique name;
   Mutex api_lock_;
   VideoCaptureCapability _requestedCapability;  // Should be set by platform
@@ -96,7 +99,6 @@ class RTC_EXPORT VideoCaptureImpl : public VideoCaptureModule {
  private:
   void UpdateFrameCount();
   uint32_t CalculateFrameRate(int64_t now_ns);
-  int32_t DeliverCapturedFrame(VideoFrame& captureFrame);
   void DeliverRawFrame(uint8_t* videoFrame,
                        size_t videoFrameLength,
                        const VideoCaptureCapability& frameInfo,
