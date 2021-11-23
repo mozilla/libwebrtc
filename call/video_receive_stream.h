bbdf9b0a6d7ea2a0b319733dc48b31f764550053
Andreas Pehrson <apehrson@mozilla.com>
2021-11-23 14:11:00 +0000

    Bug 1742181 - libwebrtc: Implement packetsDiscarded bookkeeping for received video. r=ng
    Depends on D131707
    
    Differential Revision: https://phabricator.services.mozilla.com/D131708
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/d0196a45a1f449874fc2a759e85e403c45c25575
    
    Also includes:
    
    Bug 1804288 - (fix-de7ae5755b) reimplement Bug 1742181 - libwebrtc: Implement packetsDiscarded bookkeeping for received video. r=pehrsons
    
    Differential Revision: https://phabricator.services.mozilla.com/D163959
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/ee566d1bfb654d36e5d58dce637fb0580b989ac1

--- call/video_receive_stream.h
+++ call/video_receive_stream.h
@@ -106,6 +106,8 @@ class VideoReceiveStreamInterface : public MediaReceiveStreamInterface {
     // https://www.w3.org/TR/webrtc-stats/#dom-rtcvideoreceiverstats-framesdropped
     uint32_t frames_dropped = 0;
     uint32_t frames_decoded = 0;
+    // https://w3c.github.io/webrtc-stats/#dom-rtcreceivedrtpstreamstats-packetsdiscarded
+    uint64_t packets_discarded = 0;
     // https://w3c.github.io/webrtc-stats/#dom-rtcinboundrtpstreamstats-totaldecodetime
     TimeDelta total_decode_time = TimeDelta::Zero();
     // https://w3c.github.io/webrtc-stats/#dom-rtcinboundrtpstreamstats-totalprocessingdelay
