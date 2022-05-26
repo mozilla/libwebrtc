64639aa93d9bd7b693fe0e5ddad61b14a192ca80
Michael Froman <mjfroman@mac.com>
2022-05-26 11:46:40 -0500

    Bug 1766646 - (fix-ae0d117d51) ifdef our Csrc impl vs upstream's impl, see Bug 1771332.

--- modules/rtp_rtcp/source/rtp_header_extension_map.cc
+++ modules/rtp_rtcp/source/rtp_header_extension_map.cc
@@ -53,7 +53,6 @@ constexpr ExtensionInfo kExtensions[] = {
     CreateExtensionInfo<ColorSpaceExtension>(),
     CreateExtensionInfo<InbandComfortNoiseExtension>(),
     CreateExtensionInfo<VideoFrameTrackingIdExtension>(),
-    CreateExtensionInfo<CsrcAudioLevel>(),
 };
 
 // Because of kRtpExtensionNone, NumberOfExtension is 1 bigger than the actual
