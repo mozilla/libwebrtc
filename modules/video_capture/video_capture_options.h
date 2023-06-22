743f8dda14577e7cb07a672780b3a3904cdf83f5
Nico Grunbaum <na-g@nostrum.com>
2023-06-22 16:23:00 +0000

    Bug 1837918 - libwebrtc update broke the build on OpenBSD;r=mjf,webrtc-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D181791
    Mercurial Revision: https://hg.mozilla.org/mozilla-central/rev/2a6a838b7021bb285f9485c2ceda6ba2543e0d6f

--- modules/video_capture/video_capture_options.h
+++ modules/video_capture/video_capture_options.h
@@ -55,7 +55,7 @@ class RTC_EXPORT VideoCaptureOptions {
 
   void Init(Callback* callback);
 
-#if defined(WEBRTC_LINUX)
+#if defined(WEBRTC_LINUX) || defined(WEBRTC_BSD)
   bool allow_v4l2() const { return allow_v4l2_; }
   void set_allow_v4l2(bool allow) { allow_v4l2_ = allow; }
 #endif
@@ -68,7 +68,7 @@ class RTC_EXPORT VideoCaptureOptions {
 #endif
 
  private:
-#if defined(WEBRTC_LINUX)
+#if defined(WEBRTC_LINUX) || defined(WEBRTC_BSD)
   bool allow_v4l2_ = false;
 #endif
 #if defined(WEBRTC_USE_PIPEWIRE)
